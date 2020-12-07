7dcd3306ee96873d24effad05233ebf60ce98bcc
boto-olsson <69902826+boto-olsson@users.noreply.github.com>
2020-12-07 15:06:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ p    {color: white;}
  <h1 style="font-size:300%;">This is the new application version</h1>
  </head>
  <body>
- <?php echo 'App Version: 20.0. Pod IP: '; ?>
- <?php echo $_SERVER['SERVER_ADDR']; ?>
+ <?php echo "<p> <font color=blue font face='arial' size='20pt'>'App Version: 30.0. Pod IP: '</font> </p>"; ?>
+ <?php echo "<p> <font color=blue font face='arial' size='20pt'> $_SERVER['SERVER_ADDR']; '</font> </p>" ?>
  <?php echo ' PHP Version: ' . phpversion(); ?>
  </body>
 </html>
